ad981a77aa2652703658269028c962f9abe8bdaa
Qwab17 <quadriwasiubiodun@gmail.com>
2023-08-10 09:36:36 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Beginning point
+ *
+ * Return: always 0 (successfully)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+
+}	
