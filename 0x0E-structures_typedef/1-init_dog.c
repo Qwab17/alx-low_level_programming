0eee47f9be61a3dd899cc65f06ea2ab56a58d176
qwab17 <quadriwasiubiodun@gmail.com>
2023-09-11 15:58:21 -0700

    fxn that initialize a variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - function initialize variable of type struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
