bc2bc71a0f8e099bb90217ab9ab71423048c8cab
qwab17 <quadriwasiubiodun@gmail.com>
2023-09-28 09:58:16 -0700

    fxn that convert binary to unsigned

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - function that Converts binary to unsigned integer
+ * @b: Pointer to a string
+ * Return: converted number or 0 if invalid
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int q = 0;
+	char current_char = b[q];
+
+	if (b == NULL)
+	{
+		perror("Error: Input string is NULL");
+		return (0);
+	}
+	result = 0;
+
+	for (q = 0; b[q] != '\0'; q++)
+	{
+		current_char = b[q];
+
+		if (current_char == '0')
+		{
+			result = result << 1;
+		}
+		else if (current_char == '1')
+		{
+			result = (result << 1) | 1;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (result);
+}
