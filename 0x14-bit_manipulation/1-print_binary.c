e086d191363ccfd36f7906906442e2f4ed05ba37
qwab17 <quadriwasiubiodun@gmail.com>
2023-09-28 11:40:38 -0700

    fxn that prints binary rep of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_binary - function that Prints binary rep of a number.
+ * @n: The number to be converted and printed.
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+
+	_putchar((n & 1) + '0');
+}
+
