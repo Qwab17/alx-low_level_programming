3d26e2b76bfe3a4be736b48b899d87f3be3c4607
qwab17 <quadriwasiubiodun@gmail.com>
2023-10-02 16:17:32 -0700

    fxn that copy a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+int main(int argc, char *argv[]);
+
+/**
+ * main - function that copy content of file to other
+ * @argc: arguments count
+ * @argv: argument vector
+ * Return: 0 on success, or exit error
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_source, file_dest;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s file_source file_dest\n", argv[0]);
+		exit(97);
+	}
+
+	file_source = open(argv[1], O_RDONLY);
+	if (file_source == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_dest == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((bytes_read = read(file_source, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(file_dest, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(file_source) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_source);
+		exit(100);
+	}
+
+	if (close(file_dest) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_source);
+		exit(100);
+	}
+
+	return (0);
+}
